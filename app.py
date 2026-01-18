59a8ba7611e65c6608c85407b74fbcd56c13c3c5
mikeybeezy <mikeybabs40@gmail.com>
2026-01-18 21:11:43 +0000

    test 2

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class Handler(BaseHTTPRequestHandler):
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "8080"))
     server = HTTPServer(("0.0.0.0", port), Handler)
-    print(f"GoldenPath IDP - Hello World we made it here")
+    print(f"GoldenPath IDP - Hello World we made it here again")
     print(f"Version: {VERSION}")
     print(f"Server running on :{port}")
     server.serve_forever()
